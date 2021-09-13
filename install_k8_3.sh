51cbdfae82a6f3e65565aa99c18d0497bfd91474
Rosh PR <roshpr@gmail.com>
2021-09-12 21:20:49 -0700

    Update install_k8_3.sh

--- install_k8_3.sh
+++ install_k8_3.sh
@@ -6,3 +6,6 @@ if [ $? -ne 0 ];then
 fi
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$vmip --ignore-preflight-errors=NumCPU
+
+echo "find the kubeadm config file in config map"
+echo "kubectl get configMap kubeadm-config -o yaml --namespace=kube-system"
