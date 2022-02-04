25eb53df521c2fbbd3e99a6af641fcdf2665005e
Rosh PR <roshpr@gmail.com>
2022-02-03 16:12:09 -0800

    Update install_k8_nw_5.sh

--- install_k8_nw_5.sh
+++ install_k8_nw_5.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+# Fix flannel subnet file createion error: https://github.com/kubernetes/kubernetes/issues/70202
+echo "
+FLANNEL_NETWORK=10.244.0.0/16
+FLANNEL_SUBNET=10.244.0.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true
+" > /run/flannel/subnet.env
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
