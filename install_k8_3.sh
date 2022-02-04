138245c026487466a74afb2d8f92e749d28a325a
Rosh PR <roshpr@gmail.com>
2022-02-03 16:10:36 -0800

    Update install_k8_3.sh

--- install_k8_3.sh
+++ install_k8_3.sh
@@ -4,7 +4,10 @@ vmip=$1
 if [ $? -ne 0 ];then
 	echo "Usage: <cmd> vmip"
 fi
-
+### Avoid docker systemd error: https://github.com/schoolofdevops/kubernetes-labguide/issues/10
+echo '{"exec-opts": ["native.cgroupdriver=systemd"]}' >> /etc/docker/daemon.json
+systemctl restart docker
+###
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$vmip --ignore-preflight-errors=NumCPU
 
 echo "find the kubeadm config file in config map"
